322444a5ffe37f612342e994f55b7046026eae72
Peter Reimer <peter@4pi.org>
2015-03-18 09:54:48 +0100

    logging

--- fourpi/pannellum/tile.py
+++ fourpi/pannellum/tile.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env  python
 
 from __future__ import unicode_literals, print_function
+from distutils.spawn import find_executable
+
+import logging
 import math
+import tempfile
 
 MAXIMUM_TILESIZE = 640
 MAX_LEVELS = 6
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+EXIFTOOL = find_executable('exiftool')
+
+if EXIFTOOL:
+    logger.info("exiftool found at %s" % EXIFTOOL)
+else:
+    logger.error("EXIFTOOL required but not found.")
 
 class Pannellum:
 
@@ -21,8 +39,8 @@ class Pannellum:
             tile_width = int(fragments * tile_fragment)
             exp = exp + 1
             fragments = 2 ** exp
-            #print(exp, fragments, tile_width)
         levels = int(math.log(face_width / tile_width, 2) + 1)
+        logger.info("Scaling: %s" % (face_width))
         return face_width, tile_width, levels
 
 
@@ -30,6 +48,6 @@ if __name__ == "__main__":
 
     p = Pannellum()
 
-    for pano_width in range(5000, 36000, 1000):
+    for pano_width in (3600, 5400, 8000, 11500):#range(5000, 16000, 1000):
         face, tile, levels = p.levels_and_tiles(pano_width)
         print(pano_width, levels, face, tile, face / (pano_width/math.pi))
