c03c22f7f57a882fcc1c0a8961776ba46b7bfd1a
Peter Reimer <peter@4pi.org>
2016-02-17 23:24:32 +0100

    calculate targetyaw based on bearing and northOffset of scene and hotspot

--- fourpi/pannellum/hotspot.py
+++ fourpi/pannellum/hotspot.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env  python
 
 from __future__ import print_function
-import logging
 from haversine import haversine                    
 import math
+import logging
 from fourpi.pannellum.utils import _pretty_distance
 
 logger = logging.getLogger('pannellum.hotspot')
@@ -19,11 +19,16 @@ class HotSpot:
         self.text = dest.get('title', 'n/a')
 
         self.northOffset = src.get('northOffset', 0)
+        self.targetNorthOffset = dest.get('northOffset', 0)
         
         if self.src.has_key("latlng") and self.dest.has_key("latlng"):
             self.gps = True
 
     def _get_distance(self):
+        """Return distance to hotspot in a humanfriendly way,
+            ie 123 m, 1.6 km etc.
+        """
+        
         if hasattr(self, 'gps'):
             distance = haversine(self.src['latlng'], self.dest['latlng'])
             return _pretty_distance(distance) 
@@ -31,7 +36,9 @@ class HotSpot:
             return ""
 
     def _get_bearing(self):
-
+        """Return the bearing of the hotspot, used as yaw angle. 
+        """
+        
         if hasattr(self, 'gps'):
             lat1 = self.src['latlng'][0]
             lng1 = self.src['latlng'][1]
@@ -41,23 +48,30 @@ class HotSpot:
 
             rlat1 = math.radians(lat1)
             rlat2 = math.radians(lat2)
-            rlng1 = math.radians(lng1)
-            rlng2 = math.radians(lng2)
+            # rlng1 = math.radians(lng1)
+            # rlng2 = math.radians(lng2)
             dlng = math.radians(lng2 - lng1)
 
             b = math.atan2(math.sin(dlng) * math.cos(rlat2), math.cos(rlat1) * math.sin(rlat2) - math.sin(rlat1) * math.cos(rlat2) * math.cos(dlng)) # bearing calc
             bd = math.degrees(b)
-            br, bn = divmod(bd + 360, 360) # the bearing remainder and final bearing
-            return bn - self.northOffset
+            bn = (bd + 360) % 360
+            return bn
         else:
             return 0
 
     def get_conf(self):
+        """Return the hotspots configuration dictionary.
+        """
+        
         title = self.text
+        yaw = self._get_bearing() - self.northOffset
+        targetYaw = yaw + self.northOffset - self.targetNorthOffset
         conf = {}
         conf['type'] = "scene"
         conf['text'] = "%s (%s)" % (title, self._get_distance())
-        conf['yaw'] = self._get_bearing()
+        conf['yaw'] = (yaw + 360) % 360
         conf['pitch'] = 0
+        conf['targetPitch'] = 0
+        conf['targetYaw'] = (targetYaw + 360) % 360
         conf['sceneId'] = self.scene_id
         return conf
